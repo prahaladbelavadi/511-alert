bfb36ae441c5e97b890f9dae384ad9293a70b22e
Prahalad Belavadi <prahalad@belavadi.com>
2019-10-31 13:28:04 +0530

    make a get request and render data in browser

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Storage } from '@ionic/storage';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit {
 
-  constructor() {}
+  baseUrl = 'http://api.511.org/Traffic/Events';
+  events: any;
+
+  constructor(private http: HttpClient, private storage: Storage) { }
+
+  ngOnInit() {
+    const token = this.storage.get('token');
+
+    const URL = this.baseUrl + '?api_key=' + token;
+
+    this.events = this.http.get(URL);
+  }
 
 }
